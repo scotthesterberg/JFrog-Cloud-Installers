905fd8a801b347988f060dd08c9560d9122f4a37
danielmkn <danielmi@jfrog.com>
2020-06-12 16:37:11 -0700

    ARM templates - adding xray. testing

--- AzureResourceManager/Xray/scripts/install_xray.sh
+++ AzureResourceManager/Xray/scripts/install_xray.sh
@@ -3,26 +3,25 @@ DB_URL=$(cat /var/lib/cloud/instance/user-data.txt | grep "^JDBC_STR" | sed "s/J
 DB_NAME=$(cat /var/lib/cloud/instance/user-data.txt | grep "^DB_NAME=" | sed "s/DB_NAME=//")
 DB_USER=$(cat /var/lib/cloud/instance/user-data.txt | grep "^DB_ADMIN_USER=" | sed "s/DB_ADMIN_USER=//")
 DB_PASSWORD=$(cat /var/lib/cloud/instance/user-data.txt | grep "^DB_ADMIN_PASSWD=" | sed "s/DB_ADMIN_PASSWD=//")
+DB_SERVER=$(cat /var/lib/cloud/instance/user-data.txt | grep "^DB_SERVER=" | sed "s/DB_SERVER=//")
 STORAGE_ACCT=$(cat /var/lib/cloud/instance/user-data.txt | grep "^STO_ACT_NAME=" | sed "s/STO_ACT_NAME=//")
 STORAGE_CONTAINER=$(cat /var/lib/cloud/instance/user-data.txt | grep "^STO_CTR_NAME=" | sed "s/STO_CTR_NAME=//")
 STORAGE_ACCT_KEY=$(cat /var/lib/cloud/instance/user-data.txt | grep "^STO_ACT_KEY=" | sed "s/STO_ACT_KEY=//")
 ARTIFACTORY_VERSION=$(cat /var/lib/cloud/instance/user-data.txt | grep "^ARTIFACTORY_VERSION=" | sed "s/ARTIFACTORY_VERSION=//")
 MASTER_KEY=$(cat /var/lib/cloud/instance/user-data.txt | grep "^MASTER_KEY=" | sed "s/MASTER_KEY=//")
+JOIN_KEY=$(cat /var/lib/cloud/instance/user-data.txt | grep "^JOIN_KEY=" | sed "s/JOIN_KEY=//")
 IS_PRIMARY=$(cat /var/lib/cloud/instance/user-data.txt | grep "^IS_PRIMARY=" | sed "s/IS_PRIMARY=//")
-JFROG_URL=$(cat /var/lib/cloud/instance/user-data.txt | grep "^JFROG_URL=" | sed "s/JFROG_URL=//")
+ARTIFACTORY_URL=$(cat /var/lib/cloud/instance/user-data.txt | grep "^ARTIFACTORY_URL=" | sed "s/ARTIFACTORY_URL=//")
 
 export DEBIAN_FRONTEND=noninteractive
 
 
 # Create master.key on each node
-mkdir -p /opt/jfrog/artifactory/var/etc/security/
-cat <<EOF >/opt/jfrog/artifactory/var/etc/security/master.key
+mkdir -p /opt/jfrog/xray/var/etc/security/
+cat <<EOF >/opt/jfrog/xray/var/etc/security/master.key
 ${MASTER_KEY}
 EOF
 
-
-
-# Node settings
 
 # /var/opt/jfrog/xray/etc/system.yaml
 
@@ -33,15 +32,17 @@ sed -i -e "s/#primary: true/primary: ${IS_PRIMARY}/" /var/opt/jfrog/xray/etc/sys
 sed -i -e "s/#haEnabled:/haEnabled:/" /var/opt/jfrog/xray/etc/system.yaml
 
 # Set MS SQL configuration
-cat <<EOF >>/var/opt/jfrog/artifactory/etc/system.yaml
+cat <<EOF >>/var/opt/jfrog/xray/etc/system.yaml
     ## One of: mysql, oracle, mssql, postgresql, mariadb
     ## Default: Embedded derby
       type: postgresql
       driver: org.postgresql.Driver
       url: ${DB_URL}/${DB_NAME}?sslmode=disable
-      username: ${DB_USER}
+      username: ${DB_USER}@${DB_SERVER}
       password: ${DB_PASSWORD}
-    jfrogUrl: <JFrog URL> # LB IP address? Internal IP?
+    jfrogUrl: ${ARTIFACTORY_URL}
+    security:
+      joinKey: ${JOIN_KEY}
 
 EOF
 
